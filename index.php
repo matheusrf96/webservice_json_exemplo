ff0e6dd7bb894323005a7090a6918ccf77dce3f4
Matheus <matheusrf96@gmail.com>
2018-08-30 22:11:42 -0300

    Primeiro Commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json");
+
+require_once "db.php";
+
+$db = new DB();
+
+$sql = "SELECT * FROM pessoas";
+$db->query($sql);
+$result['listaPessoas'] = $db->resultSet($sql);
+
+echo json_encode($result);
